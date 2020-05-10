7c7b6a930c538ce1b19bc0be007d710b0047ce68
Bradley Dice <bdice@bradleydice.com>
2020-05-10 16:46:47 -0500

    Add providers for linux_ppc64le and linux_aarch64. (#16)
    * Add providers for linux_ppc64le and linux_aarch64.
    
    * Bump build number, update SPDX identifier, and update URLs to where they redirect.
    
    * Add spaces for linter.
    
    * Add spaces for linter (again).
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.7.0, and conda-forge-pinning 2020.05.08.11.44.00
    
    * Disable dependency tracking.
    
    * Add make to dependencies.
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX} --enable-shared --without-guile
+./configure --prefix=${PREFIX} --enable-shared --without-guile --disable-dependency-tracking
 make
 make install
 cp src/ctl-math.h ${PREFIX}/include
